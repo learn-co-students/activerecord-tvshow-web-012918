8c899c7921e83ada72e48346a0bf94afb46fe7ed
github username <github email address>
2018-02-08 13:53:14 -0500

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,33 @@
+class Show < ActiveRecord::Base
+
+def self.highest_rating
+  Show.maximum(:rating)
+end
+
+def self.most_popular_show
+  self.find_by_rating(self.highest_rating)
+end
+
+def self.lowest_rating
+  minimum(:rating)
+end
+
+def self.least_popular_show
+  self.find_by_rating(self.lowest_rating)
+end
+
+
+def self.ratings_sum
+  sum(:rating)
+end
+
+def self.popular_shows
+  where('rating > 5')
+end
+
+def self.shows_by_alphabetical_order
+  Show.order(:name)
+end
+
+
+end
